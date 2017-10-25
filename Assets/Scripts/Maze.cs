47621a33b68afc7d489e2e46da015386f97bdd25
ColtKing <king.colt@yahoo.ca>
2017-10-24 18:35:53 -0700

    Set up a basic structure for the game with GameManager, Maze, and MazeTile classes

---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour
+{
+    public int sizeX, sizeZ;
+	public MazeTile tilePrefab;
+
+	private MazeTile[,] tiles;
+
+	// Use this for initialization
+	void Start()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/MazeManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeManager : MonoBehaviour
+{
+    public Maze mazePrefab;
+	private Maze myMaze;
+
+    private void Setup()
+    {
+        myMaze = Instantiate(mazePrefab) as Maze;
+    }
+    
+    private void Reset()
+    {
+        Destroy(myMaze.gameObject);
+        Setup();
+    }
+    
+	// Use this for initialization
+	void Start()
+    {
+		Setup();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Z))
+        {
+			Reset();
+		}
+	}
+}
