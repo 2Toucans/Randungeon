cfa64b78a6124eecc8387efd19e20790754d7038
Colt King <king.colt@yahoo.ca>
2017-11-02 00:02:04 -0700

    Nothings working aaron halp

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,10 @@ public class Enemy : MonoBehaviour
 {
     public float speed;
     private bool[,] maze;
-    private Vector2 startPos;
-    private Vector2 destPos;
-    private Vector3 startPos3D;
-    private Vector3 destPos3D;
     private bool moving;
+    private int startX, startZ;
+    private int destX, destZ;
+    private Vector3 velocity;
     private Rigidbody body;
 	// Use this for initialization
 	void Start()
@@ -21,71 +20,76 @@ public class Enemy : MonoBehaviour
 	
 	/*void FixedUpdate()
     {
-        //the maze hasn't given it's information yet
+        //the maze hasn't given the information yet
         if(maze == null)
             return;
 
-        //if he has reached the destination tile, find a new tile to go to
         if(!moving)
         {
             moving = true;
-            Debug.Log("moving set to true");
 
             List<string> validDirs = new List<string>();
-            if ((startPos.y - 1) > 0 && maze[(int)startPos.x, (int)startPos.y - 1])
+
+            if (startZ - 1 >= 0 && maze[startX, startZ - 1])
                 validDirs.Add("up");
-            if ((startPos.x + 1) < maze.GetLength(0)-1 && maze[(int)startPos.x + 1, (int)startPos.y])
+            if (startX + 1 < maze.GetLength(0) && maze[startX + 1, startZ])
                 validDirs.Add("right");
-            if ((startPos.y + 1) < maze.GetLength(1)-1 && maze[(int)startPos.x, (int)startPos.y + 1])
+            if (startZ + 1 < maze.GetLength(1) && maze[startX, startZ + 1])
                 validDirs.Add("down");
-            if ((startPos.x - 1) > 0 && maze[(int)startPos.x - 1, (int)startPos.y])
+            if (startX - 1 >= 0 && maze[startX - 1, startZ])
                 validDirs.Add("left");
-            
-            foreach(string hi in validDirs)
-                Debug.Log("validDirs: " + hi);
 
-            string chosenDir = validDirs[Random.Range(0, validDirs.Count - 1)];
+            if (startZ - 1 >= 0)
+                Debug.Log("up=" + maze[startX, startZ-1]);
+            if (startX + 1 < maze.GetLength(0))
+                Debug.Log("right=" + maze[startX+1, startZ]);
+            if (startZ + 1 < maze.GetLength(1))
+                Debug.Log("down=" + maze[startX, startZ+1]);
+            if (startX - 1 >= 0)
+                Debug.Log("left=" + maze[startX-1, startZ]);
 
-            Debug.Log("chosenDir" + chosenDir);
+            string chosenDir = "";
+            if(validDirs.Count != 0)
+                chosenDir = validDirs[Random.Range(0, validDirs.Count - 1)];
 
             switch (chosenDir)
             {
                 case "up":
-                    destPos = startPos + new Vector2(0, -2);
+                    destX = startX;
+                    destZ = startZ - 2;
                     break;
                 case "right":
-                    destPos = startPos + new Vector2(2, 0);
+                    destX = startX + 2;
+                    destZ = startZ;
                     break;
                 case "down":
-                    destPos = startPos + new Vector2(0, 2);
+                    destX = startX;
+                    destZ = startZ + 2;
                     break;
                 case "left":
-                    destPos = startPos + new Vector2(-2, 0);
+                    destX = startX - 2;
+                    destZ = startZ;
                     break;
             }
 
-            Debug.Log("destPos = " + destPos);
-            Debug.Log("startPos = " + startPos);
-            startPos3D = new Vector3(startPos.x, 0, startPos.y);
-            destPos3D = new Vector3(destPos.x, 0, destPos.y);
-
-            body.velocity = (destPos3D - startPos3D) * Time.fixedDeltaTime * speed;
-            Debug.Log("velocity = " + body.velocity);
+            velocity = new Vector3(destX-startX, 0, destZ-startZ) * speed;
+            Debug.Log(velocity);
         }
 
-        Vector2 transPos = new Vector2(transform.position.x, transform.position.z);
-        //when it has moved to or past the destination it stops 
-        if ((transPos - startPos).magnitude
-            >= (destPos - startPos).magnitude)
+        body.velocity = velocity;
+
+        Vector3 curPos = new Vector3(transform.position.x, 0, transform.position.z);
+        //Vector3 destPos = new Vector3(destX, 0, destZ);
+        Vector3 startPos = new Vector3(startX, 0, startZ);
+        if((curPos-startPos).magnitude >= 1)
         {
-            Debug.Log("found destination");
             moving = false;
-            startPos = destPos;
-            transform.position = destPos3D;
             body.velocity = Vector3.zero;
-            
+            startX = destX;
+            startZ = destZ;
+            transform.position = new Vector3(destX, transform.position.y, destZ);
         }
-	}*/
+    }*/
 
     public void setMaze(bool[,] maze)
     {
@@ -94,7 +98,7 @@ public class Enemy : MonoBehaviour
 
     public void setPosition(int x, int z)
     {
-        startPos = new Vector2(x, z);
-        startPos3D = new Vector3(x, 0, z);
+        startX = x;
+        startZ = z;
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Randungeon
 Assignment 2 for COMP7051
+
+Q for noclip
+WASD or arrow keys for movement
+Left Shift for moving down in noclip
+Space for jumping and moving up in noclip
+Home resets the player
+Z resets the maze and the player
+T toggles day/night
+F toggles fog
+
+day/night and fog stopped activating for some reason
+enemy is currently not moving
